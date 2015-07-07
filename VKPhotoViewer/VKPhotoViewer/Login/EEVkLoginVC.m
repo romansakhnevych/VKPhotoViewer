00634f271abe4134c8019962141d7f7d41aea558
lireck <lireck_for_social@ex.ua>
2015-07-07 16:34:27 +0300

    updated project (Login finished)

--- VKPhotoViewer/VKPhotoViewer/Login/EEVkLoginVC.m
+++ VKPhotoViewer/VKPhotoViewer/Login/EEVkLoginVC.m
@@ -22,6 +22,8 @@
 {
     [super viewDidLoad];
     [indicator startAnimating];
+    [[self navigationController]setNavigationBarHidden:YES];
+    
     
     if(!vkLoginWebView){
         self.vkLoginWebView = [[UIWebView alloc] initWithFrame:self.view.bounds];
@@ -30,64 +32,71 @@
         [self.view addSubview:vkLoginWebView];
     }
     
-    NSString *lauthString = [NSString stringWithFormat:@"https://oauth.vk.com/authorize?client_id=%@&scope=%@&redirect_uri=%@&display=%@&v=%@&response_tipe=token", APP_ID,SCOPE,REDIRECT_URI,DISPLAY,API_VERSION];
+    NSString *lauthString = [NSString stringWithFormat:@"https://oauth.vk.com/authorize?client_id=%@&scope=%@&redirect_uri=%@&display=%@&v=%@&response_type=token", APP_ID,SCOPE,REDIRECT_URI,DISPLAY,API_VERSION];
     NSURL *lauthUrl = [NSURL URLWithString:lauthString];
     [vkLoginWebView loadRequest:[NSURLRequest requestWithURL:lauthUrl]];
 }
 
-- (NSString*)stringBetween:(NSString *)start andString:(NSString *)end innerString:(NSString *)str
-{
-    NSScanner *lscanner = [NSScanner scannerWithString:str];
-    [lscanner setCharactersToBeSkipped:nil];
-    [lscanner scanUpToString:start intoString:NULL];
-    if([lscanner scanString:start intoString:NULL]){
-        NSString *lresult = nil;
-        if([lscanner scanUpToString:end intoString:&lresult]){
-            return lresult;
-        }
-    }
-    
-    return nil;
-}
-
-- (void)webViewDidFinishLoad:(UIWebView *)webView
-{
-    if([vkLoginWebView.request.URL.absoluteString rangeOfString:@"access_token"].location != NSNotFound){
-        
-        NSString *laccessToken = [self stringBetween:@"access_token=" andString:@"&" innerString:[[[webView request]URL]absoluteString]];
-        NSString *ltokenLifeTime = [self stringBetween:@"expires_in=" andString:@"&" innerString:[[[webView request]URL]absoluteString]];
-        NSArray *luserArray = [[[[webView request]URL]absoluteString] componentsSeparatedByString:@"&user_id="];
-        NSString *luserID = [luserArray lastObject];
-        NSLog(@"token:%@",laccessToken);
-        NSLog(@"expires_in:%@",ltokenLifeTime);
-        NSLog(@"user_id:%@",luserID);
-        
-        
-        if(luserID){
-            [[NSUserDefaults standardUserDefaults] setObject:luserID forKey:@"vkUserId"];
-        }
-        if(ltokenLifeTime){
-            [[NSUserDefaults standardUserDefaults] setObject:ltokenLifeTime forKey:@"vkTokenLifeTime"];
-        }
-        if(laccessToken){
-            [[NSUserDefaults standardUserDefaults] setObject:laccessToken forKey:@"vkAccessToken"];
-        }
-        
-        NSLog(@"vkLoginWebView response: %@",[[[vkLoginWebView request]URL]absoluteString]);
-    }
-    else if([vkLoginWebView.request.URL.absoluteString rangeOfString:@"error"].location != NSNotFound){
-        NSLog(@"error: %@", vkLoginWebView.request.URL.absoluteString);
-    }
-    
-    [indicator stopAnimating];
-    [indicator setHidden:YES];
-    
-}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Private methods
 
+- (NSString*)stringBetween:(NSString *)start andString:(NSString *)end innerString:(NSString *)str {
+    NSScanner *lscanner = [NSScanner scannerWithString:str];
+    [lscanner setCharactersToBeSkipped:nil];
+    [lscanner scanUpToString:start intoString:NULL];
+    
+    NSString *lresult = nil;
+    
+    if([lscanner scanString:start intoString:NULL]){
+        [lscanner scanUpToString:end intoString:&lresult];
+    }
+    
+    return lresult;
+}
+
+#pragma mark - UIWebView delegates
+
+
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    NSLog(@"URL - %@", webView.request.URL.absoluteString);
+    
+    if([webView.request.URL.absoluteString rangeOfString:@"access_token"].location != NSNotFound){
+        
+        NSString *laccessToken = [self stringBetween:@"access_token=" andString:@"&" innerString:[[[webView request]URL]absoluteString]];
+        NSString *ltokenLifeTime = [self stringBetween:@"expires_in=" andString:@"&" innerString:[[[webView request]URL]absoluteString]];
+        NSArray *luserArray = [[[[webView request]URL]absoluteString] componentsSeparatedByString:@"&user_id="];
+        NSString *luserID = [luserArray lastObject];
+        
+        NSLog(@"token:%@",laccessToken);
+        NSLog(@"expires_in:%@",ltokenLifeTime);
+        NSLog(@"user_id:%@",luserID);
+
+        
+        if (luserID) {
+            [[NSUserDefaults standardUserDefaults] setObject:luserID forKey:@"vkUserId"];
+        }
+        if (ltokenLifeTime) {
+            [[NSUserDefaults standardUserDefaults] setObject:ltokenLifeTime forKey:@"vkTokenLifeTime"];
+        }
+        if (laccessToken) {
+            [[NSUserDefaults standardUserDefaults] setObject:laccessToken forKey:@"vkAccessToken"];
+        }
+        [[NSUserDefaults standardUserDefaults] synchronize];
+        
+        
+        NSLog(@"vkLoginWebView response: %@",[[[vkLoginWebView request]URL]absoluteString]);
+        
+    } else if([vkLoginWebView.request.URL.absoluteString rangeOfString:@"error"].location != NSNotFound) {
+        NSLog(@"error: %@", vkLoginWebView.request.URL.absoluteString);
+    }
+    
+    [indicator stopAnimating];
+    [indicator setHidden:YES];
+}
 
 @end
