aad17ec53e15a0086aac9e673382f1d724c70dd3
lireck <lireck_for_social@ex.ua>
2015-07-07 12:15:05 +0300

    added project

---
+++ VKPhotoViewer/VKPhotoViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VKPhotoViewer
+//
+//  Created by Admin on 07.07.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
