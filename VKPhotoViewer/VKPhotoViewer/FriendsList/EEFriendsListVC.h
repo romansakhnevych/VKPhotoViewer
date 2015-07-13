f92211eadd031c3e2250957ef8c7caaac08fc043
lireck <lireck_for_social@ex.ua>
2015-07-13 07:15:10 -0700

    going to friends list

---
+++ VKPhotoViewer/VKPhotoViewer/FriendsList/EEFriendsListVC.h
@@ -0,0 +1,13 @@
+//
+//  EEFriendsListVC.h
+//  VKPhotoViewer
+//
+//  Created by admin on 7/13/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EEFriendsListVC : UITableViewController
+
+@end
---
+++ VKPhotoViewer/VKPhotoViewer/Login/EEMainVC.m
@@ -0,0 +1,37 @@
+//
+//  EEMainVC.m
+//  VKPhotoViewer
+//
+//  Created by admin on 7/13/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import "EEMainVC.h"
+
+@interface EEMainVC ()
+
+@end
+
+@implementation EEMainVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
