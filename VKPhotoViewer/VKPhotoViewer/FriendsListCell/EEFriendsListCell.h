ded4f924214bd3d1158f7fbafb97ae9b1eb71801
lireck <lireck_for_social@ex.ua>
2015-07-16 08:22:00 -0700

    loading friends by small parts with activity indicator. added custom cell for activity indicator. added custom cell for friends list

---
+++ VKPhotoViewer/VKPhotoViewer/FriendsListCell/EEFriendsListCell.h
@@ -0,0 +1,13 @@
+//
+//  EEFriendsListCell.h
+//  VKPhotoViewer
+//
+//  Created by admin on 7/16/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EEFriendsListCell : UITableViewCell
+
+@end
---
+++ VKPhotoViewer/VKPhotoViewer/LoadingCell/EELoadingCellTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  EELoadingCellTableViewCell.h
+//  VKPhotoViewer
+//
+//  Created by admin on 7/16/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EELoadingCellTableViewCell : UITableViewCell
+
+@end
