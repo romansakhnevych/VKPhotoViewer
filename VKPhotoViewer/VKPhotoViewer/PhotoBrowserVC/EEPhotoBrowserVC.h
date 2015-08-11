926274b3306aeb27f5b765981171a398a10705e9
lireck <lireck_for_social@ex.ua>
2015-08-11 09:20:32 -0700

    add photo browser

---
+++ VKPhotoViewer/VKPhotoViewer/PhotoBrowserVC/EEPhotoBrowserVC.h
@@ -0,0 +1,13 @@
+//
+//  EEPhotoBrowserVC.h
+//  VKPhotoViewer
+//
+//  Created by admin on 8/11/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EEPhotoBrowserVC : UIViewController
+
+@end
---
+++ VKPhotoViewer/VKPhotoViewer/PhotoBrowserVC/EEPhotoBrowserVC.m
@@ -0,0 +1,37 @@
+//
+//  EEPhotoBrowserVC.m
+//  VKPhotoViewer
+//
+//  Created by admin on 8/11/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import "EEPhotoBrowserVC.h"
+
+@interface EEPhotoBrowserVC ()
+
+@end
+
+@implementation EEPhotoBrowserVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
