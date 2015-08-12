b0f4baa13cf715fec04caf5c800b90618b99ad2e
lireck <lireck_for_social@ex.ua>
2015-08-12 09:29:37 -0700

    photo viewing with zooming and swiping in progress

---
+++ VKPhotoViewer/VKPhotoViewer/PhotoGalleryVC/EEPhotoGalleryVC.h
@@ -0,0 +1,13 @@
+//
+//  EEPhotoGalleryVC.h
+//  VKPhotoViewer
+//
+//  Created by admin on 8/12/15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EEPhotoGalleryVC : UIViewController
+
+@end
